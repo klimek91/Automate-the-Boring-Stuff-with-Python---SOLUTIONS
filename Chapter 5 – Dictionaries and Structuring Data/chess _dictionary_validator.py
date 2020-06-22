913d9ffde342f79996456f7ebe155526215ddd50
Klimek <klimkowskib@gmail.com>
2020-06-22 14:00:00 +0200

    chapter5 chest

--- Chapter 5 – Dictionaries and Structuring Data/chess _dictionary_validator.py
+++ Chapter 5 – Dictionaries and Structuring Data/chess _dictionary_validator.py
@@ -1,13 +1,4 @@
-dict = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking',
-        '1h': 'bking','2h': 'bking','3h': 'bking','4h': 'bking','5h': 'bking','6h': 'bking',
-        '7h': 'bking', '6h': 'bking',
-        '1h': 'bking', '1b': 'bking', '1c': 'bking', '1h': 'bking', '8h': 'bking'}
-
 def isValidChessBoard(dictionary):
-    #kings
-    if 'bking' not in dictionary.values() or 'wking' not in dictionary.values():
-        return False
-    #no more then 16 pieces each
     countb = 0
     countw = 0
     for value in dictionary.values():
@@ -15,16 +6,38 @@ def isValidChessBoard(dictionary):
             countb += 1
         if value.startswith('w'):
             countw += 1
-    if countb >= 16 or countw >= 16:
-        return False
-    #only spots from 1a to 8h
+
     list = []
     for i in range(1, 9):
         for letter in ('abcdefgh'):
             list.append(str(i) + letter)
+
+    #kings
+    if 'bking' not in dictionary.values() or 'wking' not in dictionary.values():
+        print("There is no white king or black king")
+        return False
+        #no more then 16 pieces each
+
+    elif countb >= 16 or countw >= 16:
+        print("There are to many white or\\and black pices")
+        return False
+
+    #only spots from 1a to 8h
     for key in dictionary.keys():
         if key not in list:
+            print("You choose wrong spot")
             return False
 
+    #correct names
+    for value in dictionary.values():
+        if not (value.startswith('b') or value.startswith('w')):
+            print("Pices may be only black or white")
+            return False
+        elif not (value.endswith('pawn') or value.endswith('knight') or value.endswith('bishop')\
+        or value.endswith('rook') or value.endswith('queen') or value.endswith('king')):
+            print("Only possible pieces are pawn, knight, bishop, rook, queen and king")
+            return False
 
-isValidChessBoard(dict)
+    else:
+        print("Everything is OK")
+        return True
