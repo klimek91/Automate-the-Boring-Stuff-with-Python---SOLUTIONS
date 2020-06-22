7d56d21dfe367a1485cd79b17d1847e91d9f72c1
Klimek <klimkowskib@gmail.com>
2020-06-22 11:37:14 +0200

    chapter4 comma_code, 2nd solution

--- Chapter 4 – Lists/comma_code.py
+++ Chapter 4 – Lists/comma_code.py
@@ -1,5 +1,18 @@
-spam = ['apples', 'bananas', 'tofu', 'cats', ]
+spam = ['apples', 'bananas', 'tofu', 'cats']
 
 string = ''
 
-string = ', '.join(spam[0:-1]) + ' and ' + spam[-1]
+string = ', '.join(spam[0:-1]) + ' and ' + spam[-1] + '.'
+
+
+#or II solution
+
+string2 = ''
+
+for i in range(len(spam)):
+    if i == len(spam)-2:
+        string2 += spam[i] + ' and '
+    elif i == len(spam) -1:
+        string2 += spam[i] + '.'
+    else:
+        string2 += spam[i] + ', '
