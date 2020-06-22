2a1555eac29d76da24ea83cde29c16255009977f
Klimek <klimkowskib@gmail.com>
2020-06-22 13:03:05 +0200

    chapter5 tictactoe - win condition

--- Chapter 5 – Dictionaries and Structuring Data/tic_tac_toe_board.py
+++ Chapter 5 – Dictionaries and Structuring Data/tic_tac_toe_board.py
@@ -1,4 +1,4 @@
-import random
+import random, sys
 
 board = {'topLeft' : ' ', 'topMid' : ' ', 'topRight': ' ',
          'midLeft' : ' ', 'midMid' : ' ', 'midRight': ' ',
@@ -11,6 +11,26 @@ def printBoard(board):
     print(board['midLeft'] + '|' + board['midMid'] + '|' + board['midRight'])
     print('-+-+-')
     print(board['bottomLeft'] + '|' + board['bottomMid'] + '|' + board['bottomRight'])
+
+def winCond(board, player):
+    if (board['topLeft'] == board['topMid'] == board['topRight'] == 'O')\
+    or(board['midLeft'] == board['midMid'] == board['midRight'] == 'O')\
+    or (board['bottomLeft'] == board['bottomMid'] == board['bottomRight'] == 'O')\
+    or (board['topLeft'] == board['midLeft'] == board['bottomLeft'] == 'O') \
+    or (board['topMid'] == board['midMid'] == board['bottomMid'] == 'O') \
+    or (board['topRight'] == board['midRight'] == board['bottomRight'] == 'O') \
+    or (board['topLeft'] == board['midMid'] == board['bottomRight'] == 'O') \
+    or (board['bottomLeft'] == board['midMid'] == board['topRight'] == 'O')\
+    or (board['topLeft'] == board['topMid'] == board['topRight'] == 'X') \
+    or (board['midLeft'] == board['midMid'] == board['midRight'] == 'X') \
+    or (board['bottomLeft'] == board['bottomMid'] == board['bottomRight'] == 'X') \
+    or (board['topLeft'] == board['midLeft'] == board['bottomLeft'] == 'X') \
+    or (board['topMid'] == board['midMid'] == board['bottomMid'] == 'X') \
+    or (board['topRight'] == board['midRight'] == board['bottomRight'] == 'X') \
+    or (board['topLeft'] == board['midMid'] == board['bottomRight'] == 'X') \
+    or (board['bottomLeft'] == board['midMid'] == board['topRight'] == 'X'):
+        print("Player '{}' wins".format(player))
+        sys.exit()
 
 turn = random.choice(['X', 'O'])
 
@@ -32,6 +52,7 @@ for i in range(9):
         spot = input()
     if spot in board.keys():
         board[spot] = turn
+        winCond(board,turn)
         if turn == "X":
             turn = "O"
         else:
