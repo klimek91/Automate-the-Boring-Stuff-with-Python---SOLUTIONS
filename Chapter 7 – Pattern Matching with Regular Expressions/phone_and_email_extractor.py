dcb985989c477a62af1e2288a05901ec083871e1
Klimek <klimkowskib@gmail.com>
2020-06-23 14:29:00 +0200

    chapter7 phone_email_extractor, email regex

--- Chapter 7 – Pattern Matching with Regular Expressions/phone_and_email_extractor.py
+++ Chapter 7 – Pattern Matching with Regular Expressions/phone_and_email_extractor.py
@@ -7,3 +7,11 @@ phoneRegex = re.compile(r"""(
 (\d\d\d)
 (\s | - | \.)?
 (\d\d\d)""", re.VERBOSE)
+
+emailRegex = re.compile(r"""(
+[a-zA-Z0-9_.%+-]+      #prefix
+@
+[a-zA-Z0-9-.]+      #domain
+(\.[a-zA-Z]{2,5})#after dot
+)""", re.VERBOSE)
+
