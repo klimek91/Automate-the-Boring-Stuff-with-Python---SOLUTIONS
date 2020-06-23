d9fc3019386ae3839fee4c0373c3d667ad895d18
Klimek <klimkowskib@gmail.com>
2020-06-23 14:34:59 +0200

    chapter7 phone_email_extractor

--- Chapter 7 – Pattern Matching with Regular Expressions/phone_and_email_extractor.py
+++ Chapter 7 – Pattern Matching with Regular Expressions/phone_and_email_extractor.py
@@ -1,5 +1,8 @@
 import re, pyperclip
 
+
+text = str(pyperclip.paste())
+
 phoneRegex = re.compile(r"""(
 (\(?\+?(\d\d))\))?          #country code
 (\d\d\d)             #3 first digits
@@ -15,3 +18,15 @@ emailRegex = re.compile(r"""(
 (\.[a-zA-Z]{2,5})#after dot
 )""", re.VERBOSE)
 
+matches = []
+
+for groups in phoneRegex.findall(text):
+    phoneNum = ''.join([groups[3], groups[5], groups[7]])
+    matches.append(phoneNum)
+
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+string = '\n'.join(matches)
+
+pyperclip.copy(string)
