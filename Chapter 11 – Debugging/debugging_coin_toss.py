69e5abdb9783d6f1fd26c6027df6bd8581d467c9
Klimek <klimkowskib@gmail.com>
2020-06-25 17:11:22 +0200

    chapter11 Debugging Coin Toss - fixed

--- Chapter 11 – Debugging/debugging_coin_toss.py
+++ Chapter 11 – Debugging/debugging_coin_toss.py
@@ -3,12 +3,13 @@ guess = ''
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
-toss = random.randint(0, 1) # 0 is tails, 1 is heads
+toss = random.choice(['heads','tails'])
 if toss == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guesss = input()
+    guess = input()
+    toss = random.choice(['heads', 'tails'])
     if toss == guess:
         print('You got it!')
     else:
