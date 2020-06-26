04034a48c43a287fad659e4589ada243823728e3
Klimek <klimkowskib@gmail.com>
2020-06-26 19:43:23 +0200

    chapter12 Opening 3 Search Results - finished with sys.argv

--- Chapter 12 – Web Scraping/opening_search_results.py
+++ Chapter 12 – Web Scraping/opening_search_results.py
@@ -1,14 +1,19 @@
-import requests, bs4, webbrowser
+#!python
 
-link = "https://www.google.pl/search?&q=adam+malysz"
+import requests, bs4, webbrowser, sys
 
-headers = {'User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+if len(sys.argv) > 1:
+    searched = '+'.join(sys.argv[1:])
 
-res = requests.get(link, headers=headers)
-res.raise_for_status()
+    link = "https://www.google.pl/search?&q=" + searched
 
-soup = bs4.BeautifulSoup(res.text, 'lxml')
-linkEl = soup.select(".r > a[href]")
+    headers = {'User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
-for i in range(3):
-    webbrowser.open(linkEl[i].attrs['href'])
+    res = requests.get(link, headers=headers)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    linkEl = soup.select(".r > a[href]")
+
+    for i in range(3):
+        webbrowser.open(linkEl[i].attrs['href'])
