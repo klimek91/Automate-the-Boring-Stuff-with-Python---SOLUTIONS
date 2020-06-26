1261ff117cb7b6037b9ade8a40d6e90e4e9b9c1c
Klimek <klimkowskib@gmail.com>
2020-06-26 13:33:03 +0200

    chapter12 mapIt.py with the webbrowser Module

---
+++ Chapter 12 – Web Scraping/mapIt.py
@@ -0,0 +1,11 @@
+#!python
+
+import webbrowser, sys, pyperclip
+
+
+if len(sys.argv) > 1:
+    address = '+'.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+webbrowser.open("https://www.google.pl/maps/search/" + address)
