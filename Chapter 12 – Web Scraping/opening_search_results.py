d8be0ab3585a350b43a1ccf55685499dd008fa41
Klimek <klimkowskib@gmail.com>
2020-06-26 15:40:43 +0200

    chapter12 Project: Opening All Search Results linkEl

--- Chapter 12 – Web Scraping/opening_search_results.py
+++ Chapter 12 – Web Scraping/opening_search_results.py
@@ -1,10 +1,12 @@
 import requests, bs4
 
-#link = "https://www.google.pl/search?&q=adam+malysz"
+link = "https://www.google.pl/search?&q=adam+malysz"
 
-res = requests.get(link)
+headers = {'User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+
+res = requests.get(link, headers=headers)
 res.raise_for_status()
 
 soup = bs4.BeautifulSoup(res.text, 'lxml')
-print(soup)
-linkEl = soup.select('')
+linkEl = soup.select(".r > a[href]")
+print(linkEl)
