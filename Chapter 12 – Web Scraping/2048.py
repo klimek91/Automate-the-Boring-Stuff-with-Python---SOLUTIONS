32b3f8327750f906bd718ab6ee1adb1f7974f9d5
Klimek <klimkowskib@gmail.com>
2020-06-30 16:38:07 +0200

    Chapter12 2048 try/ex added

--- Chapter 12 – Web Scraping/2048.py
+++ Chapter 12 – Web Scraping/2048.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.common.exceptions import NoSuchElementException
 
 game_link = "https://gabrielecirulli.github.io/2048/"
 browser = webdriver.Firefox()
 browser.get(game_link)
 
 html = browser.find_element_by_tag_name("html")
-time.sleep(3)
+
 while True:
-    html.send_keys(Keys.UP)
-    html.send_keys(Keys.RIGHT)
-    html.send_keys(Keys.DOWN)
-    html.send_keys(Keys.LEFT)
+    try:
+        again = browser.find_element_by_link_text("Try again")
+        again.click()
+    except NoSuchElementException:
+        html.send_keys(Keys.UP)
+        html.send_keys(Keys.RIGHT)
+        html.send_keys(Keys.DOWN)
+        html.send_keys(Keys.LEFT)
