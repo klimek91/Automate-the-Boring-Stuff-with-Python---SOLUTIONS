ef8f317b9e0d6251b3bd1aba9cd4c88100d5141a
Klimek <klimkowskib@gmail.com>
2020-06-30 16:07:40 +0200

    Chapter12 ImgSiteDownloader

--- Chapter 12 – Web Scraping/images_site_downloader.py
+++ Chapter 12 – Web Scraping/images_site_downloader.py
@@ -1,11 +1,11 @@
 import requests, bs4, os
 
 os.makedirs("images", exist_ok=True)
-#category = input("Please enter Your category ")
-#link = "https://imgur.com/search/score?q={}".format(category)
-test = "https://imgur.com/search/score?q=football+image"
+category = input("Please enter Your category ")
+link = "https://imgur.com/search/score?q={}+image".format(category)
+
 headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
-res = requests.get(test, headers=headers)
+res = requests.get(link, headers=headers)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 postEl = soup.select('.image-list-link img')
@@ -16,8 +16,10 @@ for i in range(10): #downloading first 10 images
 
     res = requests.get(imgUrl, headers=headers)
     res.raise_for_status()
-    print("Downloading {} .. ".format(imgName))
-    file = open(os.path.join("images", imgName), 'wb')
-    for chunk in res.iter_content(100000):
-        file.write(chunk)
-    file.close()
+
+    if not os.path.exists(os.path.join("images", imgName)):
+        print("Downloading {} .. ".format(imgName))
+        file = open(os.path.join("images", imgName), 'wb')
+        for chunk in res.iter_content(100000):
+            file.write(chunk)
+        file.close()
