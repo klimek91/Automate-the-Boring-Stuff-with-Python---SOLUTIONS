899647d42d9b7d4f035c4f8fd5bf424963506362
Klimek <klimkowskib@gmail.com>
2020-07-01 14:54:51 +0200

    Chapter13 Multiplication Table Maker with sys.argv

--- Chapter 13 – Working with Excel Spreadsheets/multiplication_table_maker.py
+++ Chapter 13 – Working with Excel Spreadsheets/multiplication_table_maker.py
@@ -3,16 +3,18 @@
 import openpyxl, sys
 from openpyxl.utils import get_column_letter
 
-wb = openpyxl.Workbook()
-sheet = wb.active
+if len(sys.argv) > 1:
+    wb = openpyxl.Workbook()
+    sheet = wb.active
 
-size = 10
-for col in range(1, size+1):
-    sheet['A' + str(col + 1)].value = col
-    sheet[get_column_letter(col + 1) + '1'].value = col
+    size = int(sys.argv[1]) + 1
 
-for col in range(2, size+2):
-    for row in range(2, size+2):
-        sheet[get_column_letter(col) + str(row)].value = sheet[get_column_letter(col) + str(1)].value * sheet['A'+str(row)].value
+    for row in range(1,size):
+        sheet['A'+str(row+1)] = row
+        sheet[get_column_letter(row+1)+str(1)] = row
+        for col in range(1,size):
+            sheet[get_column_letter(col+1)+str(row+1)] = row*col
 
-wb.save('multiplication_table.xlsx')
+
+    wb.save('multiplication_table.xlsx')
+    wb.close()
