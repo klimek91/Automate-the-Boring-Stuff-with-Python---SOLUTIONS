6b511f677d14a9e1cb365b2f26c6a569439f6079
Klimek <klimkowskib@gmail.com>
2020-07-01 15:25:17 +0200

    Chapter13 BlankRowIns: with sys.argv

--- Chapter 13 – Working with Excel Spreadsheets/blank_row_inserter.py
+++ Chapter 13 – Working with Excel Spreadsheets/blank_row_inserter.py
@@ -3,18 +3,20 @@
 import openpyxl, sys
 from openpyxl.utils import get_column_letter
 
-wb = openpyxl.load_workbook('test.xlsx')
-sheet = wb.active
+if len(sys.argv) > 3:
+    file = sys.argv[3]
+    wb = openpyxl.load_workbook(file)
+    sheet = wb.active
 
-n = 4
-m = 3
+    n = sys.argv[1]
+    m = sys.argv[2]
 
-for row in range(sheet.max_row+m,n,-1):
-    for col in range(1,sheet.max_column+1):
-        sheet[get_column_letter(col) + str(row)].value = sheet[get_column_letter(col) + str(row-m)].value
+    for row in range(sheet.max_row+m,n,-1):
+        for col in range(1,sheet.max_column+1):
+            sheet[get_column_letter(col) + str(row)].value = sheet[get_column_letter(col) + str(row-m)].value
 
-for row in range(n,n+m):
-    for col in range(1,sheet.max_column+1):
-        del sheet[get_column_letter(col) + str(row)]
+    for row in range(n,n+m):
+        for col in range(1,sheet.max_column+1):
+            del sheet[get_column_letter(col) + str(row)]
 
-wb.save('updTest.xlsx')
+    wb.save('new'+file)
