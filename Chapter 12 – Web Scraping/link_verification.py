540f7553ed254a4826528b6bb95702b378f9d2d6
Klimek <klimkowskib@gmail.com>
2020-07-01 11:30:02 +0200

    Chapter12 Link Verification good and bad links

--- Chapter 12 – Web Scraping/link_verification.py
+++ Chapter 12 – Web Scraping/link_verification.py
@@ -2,13 +2,12 @@ import requests, bs4
 
 headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
-url = "https://xkcd.com/"
-
+url = input("Enter URL to verify links in it: ")
 res = requests.get(url, headers=headers)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 links = soup.select('a')
-print(links)
+
 
 to_check = []
 
@@ -18,4 +17,13 @@ for link in links:
         to_check.append(url)
     elif url.startswith('//'):
         to_check.append('https:' + url)
-print(to_check)
+
+good_links = []
+bad_links = []
+
+for link in to_check:
+    result = requests.get(link)
+    if result.status_code == 404:
+        bad_links.append(link)
+    else:
+        good_links.append(link)
