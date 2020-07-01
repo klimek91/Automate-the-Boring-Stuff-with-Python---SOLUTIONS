21a2a5ad81e57061248297821863ae116792a37e
Klimek <klimkowskib@gmail.com>
2020-07-01 15:21:45 +0200

    Chapter13 BlankRowIns: add blank spots

--- Chapter 13 – Working with Excel Spreadsheets/blank_row_inserter.py
+++ Chapter 13 – Working with Excel Spreadsheets/blank_row_inserter.py
@@ -6,11 +6,15 @@ from openpyxl.utils import get_column_letter
 wb = openpyxl.load_workbook('test.xlsx')
 sheet = wb.active
 
-n = 3
-m = 2
+n = 4
+m = 3
 
 for row in range(sheet.max_row+m,n,-1):
     for col in range(1,sheet.max_column+1):
         sheet[get_column_letter(col) + str(row)].value = sheet[get_column_letter(col) + str(row-m)].value
+
+for row in range(n,n+m):
+    for col in range(1,sheet.max_column+1):
+        del sheet[get_column_letter(col) + str(row)]
 
 wb.save('updTest.xlsx')
