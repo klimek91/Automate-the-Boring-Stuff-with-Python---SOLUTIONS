cc5e8ebac71254e5f4411fc3053fd16b452654eb
Klimek <klimkowskib@gmail.com>
2020-07-02 13:21:18 +0200

    Chapter13 Spreadsheet to Text Files, all columns

--- Chapter 13 – Working with Excel Spreadsheets/spreadsheet_to_text_files.py
+++ Chapter 13 – Working with Excel Spreadsheets/spreadsheet_to_text_files.py
@@ -1,8 +1,19 @@
 import openpyxl
+from openpyxl.utils import get_column_letter
 
 wb = openpyxl.load_workbook('textToExcel.xlsx')
 sheet = wb.active
 
 for row in range(sheet.max_row):
-    file = open('textFromExcel01.txt', 'a')
-    file.write(sheet['A'+str(row+1)].value)
+    for col in range(sheet.max_column):
+        file = open('text{}.txt'.format(col), 'a')
+        file.write(str(sheet[get_column_letter(col+1) + str(row+1)].value))
+
+
+
+
+
+
+#for row in range(sheet.max_row):
+#    file = open('textFromExcel01.txt', 'a')
+#    file.write(sheet['A'+str(row+1)].value)
