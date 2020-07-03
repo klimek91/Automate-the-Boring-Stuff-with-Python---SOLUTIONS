3aea5084f5c8413e80aad644528d527289fe904e
Klimek <klimkowskib@gmail.com>
2020-07-03 15:06:00 +0200

    Chapter15 Custom Invitations as Word Documents, all guests

--- Chapter 15 – Working with PDF and Word Documents/custom_invitations_as_word_docs.py
+++ Chapter 15 – Working with PDF and Word Documents/custom_invitations_as_word_docs.py
@@ -1,17 +1,23 @@
 import docx
 
 doc = docx.Document('invitations.docx')
-
-para1 = doc.add_paragraph('It would be a pleasure to have the company of')
-para2 = doc.add_paragraph('NAME FOR TEXT')
-para3 = doc.add_paragraph('at 11010 Memory Lane on the Evening of')
-para4 = doc.add_paragraph('Aprill 1st')
-para5 = doc.add_paragraph('at 7o\'clock')
-doc.paragraphs[5].runs[0].add_break(docx.text.run.WD_BREAK.PAGE)
+file = open('guests.txt')
+guests = file.readlines()
 
 
-para1.style = para3.style = para5.style = 'text_style'
-para2.style = 'name_style'
-para4.style = 'date_style'
+for guest in guests:
+    name = guest.replace('\n','')
+
+    para1 = doc.add_paragraph('It would be a pleasure to have the company of')
+    para2 = doc.add_paragraph(name)
+    para3 = doc.add_paragraph('at 11010 Memory Lane on the Evening of')
+    para4 = doc.add_paragraph('Aprill 1st')
+    para5 = doc.add_paragraph('at 7o\'clock')
+    para5.runs[0].add_break(docx.text.run.WD_BREAK.PAGE)
+
+    para1.style = para3.style = para5.style = 'text_style'
+    para2.style = 'name_style'
+    para4.style = 'date_style'
+
 
 doc.save('invitations.docx')
