7d00816f2a2a501124b3cbdb0ee37d182b74f757
Klimek <klimkowskib@gmail.com>
2020-07-03 15:10:11 +0200

    Chapter15 Custom Invitations as Word Documents, all guests, fixed last break

--- Chapter 15 – Working with PDF and Word Documents/custom_invitations_as_word_docs.py
+++ Chapter 15 – Working with PDF and Word Documents/custom_invitations_as_word_docs.py
@@ -5,15 +5,16 @@ file = open('guests.txt')
 guests = file.readlines()
 
 
-for guest in guests:
-    name = guest.replace('\n','')
+for guestNum in range(len(guests)):
+    name = guests[guestNum].replace('\n','')
 
     para1 = doc.add_paragraph('It would be a pleasure to have the company of')
     para2 = doc.add_paragraph(name)
     para3 = doc.add_paragraph('at 11010 Memory Lane on the Evening of')
     para4 = doc.add_paragraph('Aprill 1st')
     para5 = doc.add_paragraph('at 7o\'clock')
-    para5.runs[0].add_break(docx.text.run.WD_BREAK.PAGE)
+    if guestNum < len(guests)-1:
+        para5.runs[0].add_break(docx.text.run.WD_BREAK.PAGE)
 
     para1.style = para3.style = para5.style = 'text_style'
     para2.style = 'name_style'
