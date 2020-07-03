412ae117ea5ecde12025e50df4d3f46902ebfbaa
Klimek <klimkowskib@gmail.com>
2020-07-03 12:56:09 +0200

    Chapter15 PDF Paranoia, encrypting all files

--- Chapter 15 – Working with PDF and Word Documents/pdf_paranoia_encrypt.py
+++ Chapter 15 – Working with PDF and Word Documents/pdf_paranoia_encrypt.py
@@ -1,15 +1,21 @@
 import os, PyPDF2
 
-pdfFile = open("test.pdf", 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFile)
-pdfWriter = PyPDF2.PdfFileWriter()
+for mainfolder, subfolders, filenames in os.walk(os.getcwd()):
+    for pdf in filenames:
+        if pdf.endswith('.pdf'):
+            filename = os.path.join(mainfolder, pdf)
+            pdfFile = open(filename, 'rb')
+            pdfReader = PyPDF2.PdfFileReader(pdfFile)
+            pdfWriter = PyPDF2.PdfFileWriter()
 
-for numPage in range(pdfReader.numPages):
-    pageObj = pdfReader.getPage(numPage)
-    pdfWriter.addPage(pageObj)
+            for numPage in range(pdfReader.numPages):
+                pageObj = pdfReader.getPage(numPage)
+                pdfWriter.addPage(pageObj)
 
-pdfWriter.encrypt("password")
-resultPdf = open('text_encrypted.pdf','wb')
-pdfWriter.write(resultPdf)
-resultPdf.close()
-pdfFile.close()
+
+            print("Encrypting {}.. ".format(filename))
+            pdfWriter.encrypt("password")
+            resultPdf = open(filename.replace('.pdf','_encrypted.pdf'), 'wb')
+            pdfWriter.write(resultPdf)
+            resultPdf.close()
+            pdfFile.close()
