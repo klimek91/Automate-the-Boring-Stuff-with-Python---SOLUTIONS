eacb8481e8ed654f4f7f5c29e33422493371825a
Klimek <klimkowskib@gmail.com>
2020-07-03 15:47:01 +0200

    Chapter15 Brute-Force PDF Password Breaker, with all pdfs

--- Chapter 15 – Working with PDF and Word Documents/brute-force_pdf_password_breaker.py
+++ Chapter 15 – Working with PDF and Word Documents/brute-force_pdf_password_breaker.py
@@ -1,16 +1,21 @@
-import PyPDF2
+import PyPDF2, os
 
-file = open('dictionary.txt')
+file = open('dictionary.txt') #This dictionary file contains over 44,000 English words (in capital letters) with one word per line.
 passwords = file.readlines()
 
-pdfReader = PyPDF2.PdfFileReader(open('test_encrypted.pdf', 'rb'))
-
-for password in passwords:
-    password = password.replace('\n','')
-    if pdfReader.decrypt(password) == 1:
-        print("WE FOUND THE PASSWORD: {}".format(password))
-        break
-    if pdfReader.decrypt(password.lower()) == 1:
-        print("WE FOUND THE PASSWORD: {}".format(password.lower()))
-        break
-
+for mainfolder, subfolders, filenames in os.walk(os.getcwd()):
+    for filename in filenames:
+        if filename.endswith('.pdf'):
+            file = os.path.join(mainfolder, filename)
+            pdfReader = PyPDF2.PdfFileReader(open(file, 'rb'))
+            if not pdfReader.isEncrypted:
+                print('{} is not encrypted'.format(filename))
+            else:
+                for password in passwords:
+                    password = password.replace('\n','')
+                    if pdfReader.decrypt(password) == 1:
+                        print("{} PASSWORD IS : {}".format(filename, password))
+                        break
+                    if pdfReader.decrypt(password.lower()) == 1:
+                        print("{} PASSWORD IS : {}".format(filename, password.lower()))
+                        break
