7c69df2145680e9e9a4ad2938d744b00a0177fcd
Klimek <klimkowskib@gmail.com>
2020-07-06 17:41:27 +0200

    Chapter17 Multithreaded XKCD Downloader: download few given comics

--- Chapter 17 – Keeping Time, Scheduling Tasks, and Launching Programs/multithreaded_downloader.py
+++ Chapter 17 – Keeping Time, Scheduling Tasks, and Launching Programs/multithreaded_downloader.py
@@ -3,19 +3,23 @@ import os, requests, bs4
 os.makedirs('xkcd', exist_ok=True)
 headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
-url = "https://xkcd.com/"
+def downloadXKCD(firstNumber, lastNumber):
+    for i in range(firstNumber, lastNumber):
+        url = "https://xkcd.com/{}".format(i)
 
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, 'lxml')
-imgEl = soup.select('#comic img')
+        res = requests.get(url, headers=headers)
+        res.raise_for_status()
+        soup = bs4.BeautifulSoup(res.text, 'lxml')
+        imgEl = soup.select('#comic img')
 
-imgUrl = 'https:' + imgEl[0].attrs['src']
-imgName = os.path.basename(imgUrl)
+        imgUrl = 'https:' + imgEl[0].attrs['src']
+        imgName = os.path.basename(imgUrl)
 
-resImg = requests.get(imgUrl, headers=headers)
-resImg.raise_for_status()
-imgFile = open(os.path.join('xkcd', imgName), 'wb')
-for chunk in resImg.iter_content(100000):
-    imgFile.write(chunk)
-imgFile.close()
+        resImg = requests.get(imgUrl, headers=headers)
+        resImg.raise_for_status()
+        imgFile = open(os.path.join('xkcd', imgName), 'wb')
+        for chunk in resImg.iter_content(100000):
+            imgFile.write(chunk)
+        imgFile.close()
+
+downloadXKCD(2000,2010)
