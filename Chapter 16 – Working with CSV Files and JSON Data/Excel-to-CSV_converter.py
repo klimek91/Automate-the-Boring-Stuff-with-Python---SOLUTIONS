ba90cda6e8f2bc4cf22845f394008b99c2406583
Klimek <klimkowskib@gmail.com>
2020-07-06 14:35:33 +0200

    Chapter16 Ex to csv conv: csv from excel (1file)

--- Chapter 16 – Working with CSV Files and JSON Data/Excel-to-CSV_converter.py
+++ Chapter 16 – Working with CSV Files and JSON Data/Excel-to-CSV_converter.py
@@ -1,18 +1,16 @@
-import openpyxl
+import openpyxl, csv, os
 from openpyxl.utils import get_column_letter
 
 wb = openpyxl.load_workbook('kalkulator.xlsx')
 
-print(len(wb.sheetnames))
-
 sheet = wb.active
 
-rows = []
+outFile = open('kalkulator_{}.csv'.format(wb.sheetnames[0]),'w', newline='')
+outWriter = csv.writer(outFile)
 for rowNum in range(sheet.max_row):
     row = []
     for colNum in range(sheet.max_column):
         cell = sheet[get_column_letter(colNum+1) + str(rowNum+1)].value
         row.append(cell)
-    rows.append(row)
-
-print(rows)
+    outWriter.writerow(row)
+outFile.close()
