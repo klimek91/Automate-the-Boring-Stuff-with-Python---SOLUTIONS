0170864ba16db477a26f84231ca53c9aa696a4ca
Klimek <klimkowskib@gmail.com>
2020-07-06 15:11:14 +0200

    Chapter16 Ex to csv conv: create csv file from each excel sheet

--- Chapter 16 – Working with CSV Files and JSON Data/Excel-to-CSV_converter.py
+++ Chapter 16 – Working with CSV Files and JSON Data/Excel-to-CSV_converter.py
@@ -3,17 +3,20 @@ from openpyxl.utils import get_column_letter
 
 for excelFile in os.listdir(os.getcwd()):
     if excelFile.endswith('.xlsx'):
-
         wb = openpyxl.load_workbook(excelFile)
+        for sheetname in wb.sheetnames:
+            sheet = wb[sheetname]
+            if len(wb.sheetnames) == 1:
+                outFile = open('{}.csv'.format(excelFile.replace('.xlsx','_new')),'w', newline='')
+            else:
+                outFile = open('{}_{}.csv'.format(excelFile.replace('.xlsx',''), sheetname), 'w', newline= '')
+            outWriter = csv.writer(outFile)
 
-        sheet = wb.active
-        print(sheet.max_row, sheet.max_column)
-        outFile = open('{}.csv'.format(excelFile.replace('.xlsx','_new.csv')),'w', newline='')
-        outWriter = csv.writer(outFile)
-        for rowNum in range(sheet.max_row):
-            row = []
-            for colNum in range(sheet.max_column):
-                cell = sheet[get_column_letter(colNum+1) + str(rowNum+1)].value
-                row.append(cell)
-            outWriter.writerow(row)
-        outFile.close()
+            for rowNum in range(sheet.max_row):
+                row = []
+                for colNum in range(sheet.max_column):
+                    cell = sheet[get_column_letter(colNum+1) + str(rowNum+1)].value
+                    row.append(cell)
+                outWriter.writerow(row)
+            outFile.close()
+        wb.close()
