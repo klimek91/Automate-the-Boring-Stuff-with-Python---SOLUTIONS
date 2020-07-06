234e722b22ae75a0e7773783d4b3933db7a426c1
Klimek <klimkowskib@gmail.com>
2020-07-06 15:01:51 +0200

    Chapter16 Ex to csv conv: csv from excel (all files)

--- Chapter 16 – Working with CSV Files and JSON Data/Excel-to-CSV_converter.py
+++ Chapter 16 – Working with CSV Files and JSON Data/Excel-to-CSV_converter.py
@@ -1,16 +1,19 @@
 import openpyxl, csv, os
 from openpyxl.utils import get_column_letter
 
-wb = openpyxl.load_workbook('kalkulator.xlsx')
+for excelFile in os.listdir(os.getcwd()):
+    if excelFile.endswith('.xlsx'):
 
-sheet = wb.active
+        wb = openpyxl.load_workbook(excelFile)
 
-outFile = open('kalkulator_{}.csv'.format(wb.sheetnames[0]),'w', newline='')
-outWriter = csv.writer(outFile)
-for rowNum in range(sheet.max_row):
-    row = []
-    for colNum in range(sheet.max_column):
-        cell = sheet[get_column_letter(colNum+1) + str(rowNum+1)].value
-        row.append(cell)
-    outWriter.writerow(row)
-outFile.close()
+        sheet = wb.active
+        print(sheet.max_row, sheet.max_column)
+        outFile = open('{}.csv'.format(excelFile.replace('.xlsx','_new.csv')),'w', newline='')
+        outWriter = csv.writer(outFile)
+        for rowNum in range(sheet.max_row):
+            row = []
+            for colNum in range(sheet.max_column):
+                cell = sheet[get_column_letter(colNum+1) + str(rowNum+1)].value
+                row.append(cell)
+            outWriter.writerow(row)
+        outFile.close()
