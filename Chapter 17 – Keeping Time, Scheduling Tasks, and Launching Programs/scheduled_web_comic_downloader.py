a351537459dde4c7d854921326eb4827d75ea8d7
Klimek <klimkowskib@gmail.com>
2020-07-07 13:23:08 +0200

    Chapter17 Scheduled Web Comic Downloader: downloading all comics

--- Chapter 17 – Keeping Time, Scheduling Tasks, and Launching Programs/scheduled_web_comic_downloader.py
+++ Chapter 17 – Keeping Time, Scheduling Tasks, and Launching Programs/scheduled_web_comic_downloader.py
@@ -7,17 +7,25 @@ url = 'http://www.lefthandedtoons.com/'
 res = requests.get(url, headers=headers)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, 'lxml')
-imgEl = soup.select(".comicimage")
-imgUrl = imgEl[0].attrs['src']
-imgName = os.path.basename(imgUrl)
-
-resImg = requests.get(imgUrl, headers=headers)
-resImg.raise_for_status()
-img = open(os.path.join('comic', imgName), 'wb')
-for chunk in resImg.iter_content(100000):
-    img.write(chunk)
-img.close()
 
 prevEl = soup.select(".prev a")
-prevUrl = url + (prevEl[0].attrs['href'].replace('/',''))
-print(prevUrl)
+prevId = prevEl[0].attrs['href'].replace('/','')
+prevUrl = url + prevId
+mainUrl = str(int(prevId) + 1)
+
+while True:
+    resImg = requests.get(url + mainUrl, headers=headers)
+    resImg.raise_for_status()
+    soup = bs4.BeautifulSoup(resImg.text, 'lxml')
+    imgEl = soup.select(".comicimage")
+    imgUrl = imgEl[0].attrs['src']
+    imgName = os.path.basename(imgUrl)
+    img = open(os.path.join('comic', imgName), 'wb')
+    for chunk in resImg.iter_content(100000):
+        img.write(chunk)
+    img.close()
+    mainUrl = str(int(mainUrl) - 1)
+
+
+
+
