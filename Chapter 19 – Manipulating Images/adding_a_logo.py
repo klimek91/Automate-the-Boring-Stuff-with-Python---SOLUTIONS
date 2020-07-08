1c6408b99576da20efb5c30b0137e4a01dfaf20f
Klimek <klimkowskib@gmail.com>
2020-07-08 14:14:08 +0200

    Chapter18 Adding a Logo to all images in dir

--- Chapter 19 – Manipulating Images/adding_a_logo.py
+++ Chapter 19 – Manipulating Images/adding_a_logo.py
@@ -2,14 +2,14 @@ from PIL import Image
 import os
 
 logo = Image.open('catlogo.png')
+wlogo, hlogo = logo.size
+os.makedirs('logo', exist_ok=True)
 
 for image in os.listdir():
     if (image.endswith('.png') or image.endswith('.jpg')) and image != 'catlogo.png':
         img = Image.open(image)
         width, height = img.size
         if width > 300 or height > 300:
-
-
             if width > height:
                 img = img.resize((300, int((height*300)/width)))
                 img.save(image)
@@ -18,3 +18,6 @@ for image in os.listdir():
                 img = img.resize((int((width*300)/height), 300))
                 img.save(image)
 
+        width, height = img.size
+        img.paste(logo,(width-wlogo,height-hlogo), logo)
+        img.save(os.path.join('logo',image))
