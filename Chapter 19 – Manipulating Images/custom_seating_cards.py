72a95813927cc373727e63abe5f442c91a751840
Klimek <klimkowskib@gmail.com>
2020-07-09 12:12:55 +0200

    Chapter19 Custom Seating Cards

--- Chapter 19 – Manipulating Images/custom_seating_cards.py
+++ Chapter 19 – Manipulating Images/custom_seating_cards.py
@@ -1,11 +1,18 @@
 from PIL import Image, ImageFont, ImageDraw
 import os
 
-img = Image.open("flowers.jpg")
-img = img.resize((288,360))
-img.save('flowers.jpg')
-
-draw = ImageDraw.Draw(img)
 
 fontsFolder = 'C:\Windows\Fonts'
 arialFont = ImageFont.truetype(os.path.join(fontsFolder,'arial.ttf'),26)
+
+file = open('guests.txt', encoding='utf8')
+names = file.readlines()
+
+for i in range(len(names)):
+    img = Image.open("flowers.jpg")
+    img = img.resize((288, 360))
+    img.save('flowers.jpg')
+    draw = ImageDraw.Draw(img)
+
+    draw.text((10,150), names[i].replace('\n',''), fill='black', font=arialFont)
+    img.save('invitation{}.jpg'.format(i))
